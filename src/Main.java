7cb8b3ee9d189c111b1c05d57f9f5b9d7c616900
Fodi <Fodor.Krisztian_18b@bolyai-szakkozep.hu>
2022-12-19 17:09:32 +0100

    1/2. feladat

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+class Versenytav {
+    private String rajtSzam;
+
+    public String getTav() {
+        switch (rajtSzam.charAt(0)) {
+            case 'M':
+                return "Mini";
+            case 'R':
+                return "Rövid";
+            case 'K':
+                return "Közép";
+            case 'H':
+                return "Hosszú";
+            case 'E':
+            default:
+                return "Pedelec";
+        }
+    }
+
+    public Versenytav(String rajtszam) {
+        rajtSzam = rajtszam;
+    }
+}
+
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Hello world!");
+        }
+    }
